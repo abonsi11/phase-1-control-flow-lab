3684b13f50a1acc88807dd1e9ebaa84a03336a23
Anthony Bonsi <xbonsi96@gmail.com>
2021-10-19 14:41:07 -0600

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-function scuberGreetingForFeet(){
-  if (scuberGreetingForFeet) <= "400" 
-  return 'This one is on me!'
-
-
+function scuberGreetingForFeet(feet){
+  if (feet <= 400){
+    return 'This one is on me!'
+  } 
+  else if (feet >= 2000){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (feet > 2500){
+    return 'No can do.'
+  }
+    
+  
 
 function ternaryCheckCity(){
   // Write your code here!
@@ -10,4 +17,4 @@ function ternaryCheckCity(){
 
 function switchOnCharmFromTip(){
   // Write your code here!
-}
+}}
